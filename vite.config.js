9f595c3bd0dad3483923a08defc06628e2c207bd
zhengXiaoHuan <1574067899@qq.com>
2025-09-29 15:10:21 +0800

    add: 打包

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import vue from '@vitejs/plugin-vue'
-import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite'
+import {fileURLToPath, URL} from 'node:url'
+import {defineConfig} from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,6 +10,9 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  build: {
+    outDir: 'docs' // 指定打包输出目录为docs
   },
   server: {
     proxy: {
