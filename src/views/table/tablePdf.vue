3f840c6a0b0c9c0e3aa77517e4818c07e7096bdc
zhengXiaoHuan <1574067899@qq.com>
2025-09-29 16:21:42 +0800

    add: 增加编辑功能

--- src/views/table/tablePdf.vue
+++ src/views/table/tablePdf.vue
@@ -1,4 +1,10 @@
 <template>
+  <el-switch
+    v-model="isEdit"
+    class="switch-edit"
+    active-text="开启编辑"
+    inactive-text="关闭编辑"
+  />
   <div class="width height flex flex-column flex-center">
     <div id="table" class="table-content padding-l50 padding-r50">
       <table class="table-box">
@@ -12,26 +18,36 @@
             客户名稱
           </td>
           <td width="181px">
-            123123123123123123123123123
+            <el-input v-if="isEdit" v-model="tableData.customerName" />
+            <span v-else>{{ tableData.customerName }}</span>
           </td>
         </tr>
         <tr>
           <td>
             貨品名稱
           </td>
-          <td />
+          <td>
+            <el-input v-if="isEdit" v-model="tableData.productName" />
+            <span v-else>{{ tableData.productName }}</span>
+          </td>
         </tr>
         <tr>
           <td>
             司稱员
           </td>
-          <td />
+          <td>
+            <el-input v-if="isEdit" v-model="tableData.scalesName" />
+            <span v-else>{{ tableData.scalesName }}</span>
+          </td>
         </tr>
         <tr>
           <td>
             主管
           </td>
-          <td />
+          <td>
+            <el-input v-if="isEdit" v-model="tableData.Supervisor" />
+            <span v-else>{{ tableData.Supervisor }}</span>
+          </td>
         </tr>
       </table>
       <div class="table-gap-text padding-y5">
@@ -43,72 +59,124 @@
             序號
           </td>
           <td width="181px">
-            585885
+            <el-input v-if="isEdit" v-model="tableData.number" />
+            <span v-else>{{ tableData.number }}</span>
           </td>
         </tr>
         <tr>
           <td>
             日期
           </td>
-          <td />
+          <td>
+            <el-input v-if="isEdit" v-model="tableData.date" />
+            <span v-else>{{ tableData.date }}</span>
+          </td>
         </tr>
         <tr>
           <td>
             入廠时间
           </td>
-          <td />
+          <td>
+            <el-input v-if="isEdit" v-model="tableData.entryTime" />
+            <span v-else>{{ tableData.entryTime }}</span>
+          </td>
         </tr>
         <tr>
           <td>
             出廠时间
           </td>
-          <td />
+          <td>
+            <el-input v-if="isEdit" v-model="tableData.factoryTime" />
+            <span v-else>{{ tableData.factoryTime }}</span>
+          </td>
         </tr>
         <tr>
           <td>
             車號
           </td>
-          <td />
+          <td>
+            <el-input v-if="isEdit" v-model="tableData.carNumber" />
+            <span v-else>{{ tableData.carNumber }}</span>
+          </td>
         </tr>
         <tr>
           <td>
             客户代號
           </td>
-          <td />
+          <td>
+            <el-input v-if="isEdit" v-model="tableData.customerNumber" />
+            <span v-else>{{ tableData.customerNumber }}</span>
+          </td>
         </tr>
         <tr>
           <td>
             收費(物料)
           </td>
-          <td />
+          <td>
+            <el-input v-if="isEdit" v-model="tableData.charge" />
+            <span v-else>{{ tableData.charge }}</span>
+          </td>
         </tr>
         <tr>
           <td>
             總重
           </td>
-          <td />
+          <td>
+            <el-input v-if="isEdit" v-model="tableData.totalWeight" />
+            <span v-else>{{ tableData.totalWeight }}</span>
+          </td>
         </tr>
         <tr>
           <td>
             空重
           </td>
-          <td />
+          <td>
+            <el-input v-if="isEdit" v-model="tableData.emptyWeight" />
+            <span v-else>{{ tableData.emptyWeight }}</span>
+          </td>
         </tr>
         <tr>
           <td>
             净重
           </td>
-          <td />
+          <td>
+            <el-input v-if="isEdit" v-model="tableData.netWeight" />
+            <span v-else>{{ tableData.netWeight }}</span>
+          </td>
         </tr>
       </table>
     </div>
   </div>
-  <el-button class="export-btn" type="primary" @click="htmlToPdf.getPdf('table', '#table')">
+  <el-button :disabled="isEdit" class="export-btn" type="primary" @click="exportClick">
     打印
   </el-button>
 </template>
 <script setup>
 import htmlToPdf from '@/utils/exportPdf.js'
+import timeTool from '@/utils/timeTools.js'
+import {reactive, ref} from 'vue'
+
+const isEdit = ref(false)
+const tableData = reactive({
+  customerName: '', // 客户名称
+  productName: '', // 产品名称
+  scalesName: '', // 司称员
+  Supervisor: '', // 主管
+  number: '', // 序号
+  date: '', // 日期
+  entryTime: '', // 入厂时间
+  factoryTime: '', // 出厂时间
+  carNumber: '', // 车号
+  customerNumber: '', // 客户代號
+  charge: '', // 收费(物料)
+  totalWeight: '', // 总重
+  emptyWeight: '', // 空重
+  netWeight: '' // 净重
+})
+const exportClick = () => {
+  const name = '车牌' + tableData.carNumber + '-' + timeTool.formatTime(new Date())
+  htmlToPdf.getPdf(name, '#table')
+}
 </script>
 <style scoped lang="less">
 .table-box {
@@ -131,6 +199,9 @@ import htmlToPdf from '@/utils/exportPdf.js'
     font-size: 20px;
     padding: 5px 0;
   }
+  :deep(.el-input__wrapper) {
+    box-shadow: none;
+  }
 }
 .padding-r50 {
   padding-right: 50px;
@@ -152,5 +223,11 @@ import htmlToPdf from '@/utils/exportPdf.js'
   transform: translateX(-50%);
   left: 50%;
   width: 100px;
+}
+.switch-edit {
+  position: absolute;
+  top: 20px;
+  transform: translateX(-50%);
+  left: 50%;
 }
 </style>
